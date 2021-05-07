2b76a3662e1f05f18efc42696ba164ce7ccf2028
Gabriel Soudry <gabriel_s@hotmail.fr>
2021-05-07 08:37:39 +0200

    :sparkles: Add binding cosmosdb

--- IoTHub-ETL/index.ts
+++ IoTHub-ETL/index.ts
@@ -2,10 +2,16 @@ import { AzureFunction, Context } from "@azure/functions"
 
 const IoTHubTrigger: AzureFunction = async function (context: Context, IoTHubMessages: any[]): Promise<void> {
     context.log(`Eventhub trigger function called for message array: ${IoTHubMessages}`);
-    
+        
     IoTHubMessages.forEach(message => {
-        context.log(`Processed message: ${message}`);
+        context.log(`Processed  message: ${message}`);
+        context.bindings.outputDocument = message;
     });
+
+    context.res = {
+        status: 200,
+        body: 'Successfully persisted document'
+    };
 };
 
 export default IoTHubTrigger;
