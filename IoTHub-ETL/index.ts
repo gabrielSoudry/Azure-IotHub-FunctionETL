342bdb3690c783b2e6169a0a9cc02e5787a4a209
Gabriel Soudry <gabriel_s@hotmail.fr>
2021-05-10 10:47:39 +0200

    :building_construction: Migration EventHub => EventGrid

--- IoTHub-ETL/index.ts
+++ IoTHub-ETL/index.ts
@@ -1,17 +1,12 @@
 import { AzureFunction, Context } from "@azure/functions"
 
-const IoTHubTrigger: AzureFunction = async function (context: Context, IoTHubMessages: any[]): Promise<void> {
-    context.log(`Eventhub trigger function called for message array: ${IoTHubMessages}`);
-        
-    IoTHubMessages.forEach(message => {
-        context.log(`Processed  message: ${message}`);
-        context.bindings.outputDocument = message;
-    });
-
+const eventGridTrigger: AzureFunction = async function (context: Context, eventGridEvent: any): Promise<void> {
+    context.log(eventGridEvent.data.body);
+    context.bindings.outputDocument = eventGridEvent.data.body;
     context.res = {
         status: 200,
         body: 'Successfully persisted document'
     };
 };
 
-export default IoTHubTrigger;
+export default eventGridTrigger;
